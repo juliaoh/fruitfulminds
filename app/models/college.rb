b7578e50256f5685c87bba3cc22eb1c32b976cae
skxu <skx@berkeley.edu>
2013-10-27 17:33:52 -0700

    college migrate

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,6 +1,5 @@
 class College < ActiveRecord::Base
-  has_many :college_semesters
-  has_many :users, :through => :college_semesters
+  has_many :users
   validates :name, :presence => true
   strip_attributes
 
