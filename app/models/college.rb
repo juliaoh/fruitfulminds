680f06595a6f09ff2f0b1429eaba0d1fdcad647f
skxu <skx@berkeley.edu>
2013-10-27 17:23:34 -0700

    college model

---
+++ app/models/college.rb
@@ -0,0 +1,8 @@
+class college < ActiveRecord::Base
+  has_many :college_semesters
+  has_many :users, :through => :college_semesters
+  validates :name, :presence => true
+  strip_attributes
+
+
+end
