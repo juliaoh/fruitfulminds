49bb40856447c07d38da60ea2f53996e097bbd12
William Yau <yauyinx@gmail.com>
2013-11-10 04:18:06 -0800

    An almost working view for survey template

--- app/controllers/survey_template_controller.rb
+++ app/controllers/survey_template_controller.rb
@@ -1,5 +1,11 @@
 class SurveyTemplateController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+  def index
+  end
   def new
-
+  end
+  def create
+    flash[:notice] = params.to_s
+    redirect_to "/survey_template"
   end
 end
