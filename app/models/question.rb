01bc5eed8076a72f59f0535262d77ff854ceee9e
Alvin Wong <alwong8@berkeley.edu>
2013-11-10 01:00:42 -0800

    fix question and section model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,20 +1,10 @@
 class Question < ActiveRecord::Base
   validates :name, :presence => true
   validates :type, :presence => true, :inclusion => ["Efficacy", "Multiple Choice"]
-  validates :msg1, :presence => true, :if => "is_efficacy?"
-  validates :msg2, :presence => true, :if => "is_efficacy?"
-  validates :msg1, :inclusion => [nil], :if => "is_not_efficacy?"
-  validates :msg2, :inclusion => [nil], :if => "is_not_efficacy?"
+  validates :msg1, :presence => true
+  validates :msg2, :presence => true
 
   belongs_to :section
   belongs_to :curriculum
-
-  def is_efficacy?
-    type == "Efficacy"
-  end
-
-  def is_not_efficacy?
-    !is_efficacy?
-  end
 
 end
