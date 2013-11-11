e60447a64219d253a63d8b0a453d2a94fb306053
Alvin Wong <alwong8@berkeley.edu>
2013-11-10 21:53:12 -0800

    Fix Pre/Post Survey Model

--- app/models/presurvey.rb
+++ app/models/presurvey.rb
@@ -3,7 +3,11 @@ class Presurvey < ActiveRecord::Base
   belongs_to :user
   belongs_to :school_semester
   serialize :data
+  serialize :total
   validates_presence_of :school_semester_id
+  validates :number_students, :presence => true
+  validates :curriculum_id, :presence => true
+  validates :course_id, :presence => true
 
   delegate :school_name_and_semester, :to => :school_semester
 end
