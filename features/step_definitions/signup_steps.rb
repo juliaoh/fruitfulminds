87d8b9299927e28e9e89820be1a9385c5ae1866d
William Yau <yauyinx@gmail.com>
2013-11-10 23:30:55 -0800

    added Terms of Service cucumber test

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,3 +1,14 @@
 Then /^(?:|I )should see "([^"]*)" in drop-down menu$/ do |text|
   page.all(:css, 'select#' + text).length.should == 1
 end
+
+Then /^I should see the Terms of Service on a new page$/ do
+  page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)
+  current_path = URI.parse(current_url).path
+  correct_path = URI.parse("/services").path
+  if current_path.respond_to? :should
+    current_path.should == correct_path
+  else
+    assert_equal correct_path, current_path
+  end
+end
