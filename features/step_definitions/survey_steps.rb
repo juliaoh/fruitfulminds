b30a8684d9d9b2b4208766bb84ed7c18645f462b
skxu <skx@berkeley.edu>
2013-11-10 22:10:14 -0800

    added some steps

--- features/step_definitions/survey_steps.rb
+++ features/step_definitions/survey_steps.rb
@@ -17,3 +17,34 @@ Given /the following efficacies exist/ do |results_table|
     Efficacy.create!(result)
   end
 end
+
+
+Given /the following curriculum exist/ do |table|
+  table.hashes.each do |result|
+    name = result[:name]
+    Curriculum.create!(:name=>name)
+  end
+end
+
+
+Given /the following sections exist/ do |table|
+  table.hashes.each do |result|
+    name = result[:name]
+    objective = result[:objective]
+    stype = result[:stype]
+    curriculum_id = result[:curriculum_id]
+    Section.create!(:name=>name,:objective=>objective,:stype=>stype,:curriculum_id=>curriculum_id)
+  end
+end
+
+
+Given /the following questions exist/ do |table|
+  table.hashes.each do |result|
+    name = result[:name]
+    qtype = result[:qtype]
+    msg1 = result[:msg1]
+    msg2 = result[:msg2]
+    section_id = result[:section_id]
+    Question.create!(:name=>name, :qtype=>qtype, :msg1=>msg1, :msg2=>msg2, :section_id=>section_id)
+  end
+end
