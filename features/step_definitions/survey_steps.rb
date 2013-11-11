acdada3b7cfebf8ec5bc19e50fe4fcda4a04eee7
skxu <skx@berkeley.edu>
2013-11-11 01:05:21 -0800

    surveys_steps

--- features/step_definitions/survey_steps.rb
+++ features/step_definitions/survey_steps.rb
@@ -1,8 +1,12 @@
-Given /the following pre-results exist/ do |results_table|
+Given /the following pre-results 1 exist/ do |results_table|
   results_table.hashes.each do |result|
-    school_id = result[:school_semester_id]
-    result.delete(:school_semeseter_id)
-    Presurvey.create!(:school_semester_id=>school_id, :data=>result)
+    Presurvey::Part1.create!(result)
+  end
+end
+
+Given /the following pre-results 2 exist/ do |results_table|
+  results_table.hashes.each do |result|
+    Presurvey::Part2.create!(result)
   end
 end
 
