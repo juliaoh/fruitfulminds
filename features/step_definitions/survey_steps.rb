e74f0d322ddb1e1f57f8d1beb4bd4742cdf8b50b
skxu <skx@berkeley.edu>
2013-11-11 01:06:46 -0800

    Revert "surveys_steps"
    This reverts commit acdada3b7cfebf8ec5bc19e50fe4fcda4a04eee7.

--- features/step_definitions/survey_steps.rb
+++ features/step_definitions/survey_steps.rb
@@ -1,12 +1,8 @@
-Given /the following pre-results 1 exist/ do |results_table|
+Given /the following pre-results exist/ do |results_table|
   results_table.hashes.each do |result|
-    Presurvey::Part1.create!(result)
-  end
-end
-
-Given /the following pre-results 2 exist/ do |results_table|
-  results_table.hashes.each do |result|
-    Presurvey::Part2.create!(result)
+    school_id = result[:school_semester_id]
+    result.delete(:school_semeseter_id)
+    Presurvey.create!(:school_semester_id=>school_id, :data=>result)
   end
 end
 
