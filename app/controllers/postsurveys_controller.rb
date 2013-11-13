ccf52fdcc7d4955e2350fcad84dbd86f2f4e7658
Kevin <kevinyeun@berkeley.edu>
2013-11-12 23:29:41 -0800

    glory to thy presurveys+postsurveys

--- app/controllers/postsurveys_controller.rb
+++ app/controllers/postsurveys_controller.rb
@@ -1,47 +1,42 @@
 class PostsurveysController < ApplicationController
-  def new
-    @all_schools = School.all
-    @postsurvey_fields = Postsurvey.new(params[:postsurvey])
-    @efficacy_fields = Efficacy.new(params[:efficacy])
-  end
-
-  def create
-    if params[:admin]
-      school = School.find(params[:admin][:school_id])
-    else
-      school = School.find(@school)
-    end
-    begin
-      ps = @current_user.postsurveys.new
-      ps.school_semester_id = SchoolSemester.find(school).id
-      ps.update_attributes!(params[:postsurvey])
-      eff = Efficacy.new
-      eff.postsurvey_id = ps.id
-      eff.number_students = params[:postsurvey][:number_students]
-      eff.update_attributes!(params[:efficacy])
-      flash[:notice] = "Results successfully added."
-      redirect_to portal_path
-    rescue ActiveRecord::RecordInvalid
-      flash[:warning] = "Results failed to add. Incomplete or has invalid characters."
-      redirect_to new_postsurvey_path(:postsurvey => params[:postsurvey], :efficacy => params[:efficacy])
-    end
-  end
-
   def edit
-    @postsurvey_fields = Postsurvey.find(params[:id])
-    @efficacy_fields = Efficacy.find_by_postsurvey_id(@postsurvey_fields.id)
+    postsurvey = Postsurvey.find_by_id(params[:id])
+    if params[:failed_data]
+      @postsurvey_data = {}
+      params[:failed_data].each do |qid, num|
+        @postsurvey_data[Integer(qid)] = num
+      end
+    else
+      @postsurvey_data = postsurvey.data
+    end
+    @curriculum = Curriculum.find_by_id(postsurvey.curriculum_id)
   end
   
   def update
     begin
-      Postsurvey.find(params[:id]).update_attributes!(params[:postsurvey])
-      Efficacy.find_by_postsurvey_id(params[:id]).update_attributes!(:number_students => params[:postsurvey][:number_students])
-      Efficacy.find_by_postsurvey_id(params[:id]).update_attributes!(params[:efficacy])
+      postsurvey = Postsurvey.find_by_id(params[:id])
+      curriculum = Curriculum.find_by_id(postsurvey.curriculum_id)
+      new_data = {}
+      curriculum.sections.each do |section|
+        section.questions.each do |question|
+          new_data[question.id] = params[question.id.to_s]
+        end
+      end
+      new_data.each do |qid, num|
+        new_data[qid] = Integer(new_data[qid])
+      end
+      postsurvey.data = new_data
+      postsurvey.save!
       flash[:notice] = "Survey updated successfully."
-      redirect_to portal_path
-    rescue ActiveRecord::RecordInvalid
+      redirect_to postsurvey_path(:id => params[:id])
+    rescue ArgumentError
       flash[:warning] = "Results failed to add. Incomplete or has invalid characters."
-      redirect_to edit_postsurvey_path(:id => params[:id], :postsurvey => params[:postsurvey])
+      redirect_to edit_postsurvey_path(:failed_data => new_data)
     end
+  end
+
+  def show
+    @postsurvey = Postsurvey.find_by_id(params[:id])
+    @curriculum = Curriculum.find_by_id(@postsurvey.curriculum_id)
   end
 end
--- app/controllers/presurveys_controller.rb
+++ app/controllers/presurveys_controller.rb
@@ -1,52 +1,37 @@
 class PresurveysController < ApplicationController
-  def new
-    @all_schools = School.all.collect { |s| ["#{s.name}", s.id] }
-  end
-
-  def create
-    if params[:admin]
-      school = School.find(params[:admin][:school_id])
-    else
-      school = School.find(@school)
-    end
-    begin
-      ps = @current_user.presurveys.new
-      ps.school_semester_id = SchoolSemester.find(school).id
-      ps.update_attributes!(:data=>params[:presurvey])
-      eff = Efficacy.new
-      eff.presurvey_id = ps.id
-      eff.number_students = params[:presurvey][:number_students]
-      eff.update_attributes!(params[:efficacy])
-      flash[:notice] = "Results successfully added."
-      redirect_to portal_path
-    rescue ActiveRecord::RecordInvalid
-      flash[:warning] = "Results failed to add. Incomplete or has invalid characters."
-      redirect_to new_presurvey_path(:presurvey => params[:presurvey], :efficacy => params[:efficacy])
-    end
-  end
-
   def edit
-    @presurvey = Presurvey.find_by_id(params[:id])
-    @curriculum = Curriculum.find_by_id(@presurvey.curriculum_id)
+    presurvey = Presurvey.find_by_id(params[:id])
+    if params[:failed_data]
+      @presurvey_data = {}
+      params[:failed_data].each do |qid, num|
+        @presurvey_data[Integer(qid)] = num
+      end
+    else
+      @presurvey_data = presurvey.data
+    end
+    @curriculum = Curriculum.find_by_id(presurvey.curriculum_id)
   end
   
   def update
     begin
       presurvey = Presurvey.find_by_id(params[:id])
       curriculum = Curriculum.find_by_id(presurvey.curriculum_id)
-      fdata = {}
+      new_data = {}
       curriculum.sections.each do |section|
         section.questions.each do |question|
-          fdata[question.id.to_s] = params[question.id.to_s]
+          new_data[question.id] = params[question.id.to_s]
         end
       end
-      presurvey.data = fdata
+      new_data.each do |qid, num|
+        new_data[qid] = Integer(new_data[qid])
+      end
+      presurvey.data = new_data
       presurvey.save!
       flash[:notice] = "Survey updated successfully."
       redirect_to presurvey_path(:id => params[:id])
-    rescue ActiveRecord::RecordInvalid
+    rescue ArgumentError
       flash[:warning] = "Results failed to add. Incomplete or has invalid characters."
-      redirect_to edit_presurvey_path
+      redirect_to edit_presurvey_path(:failed_data => new_data)
     end
   end
 
