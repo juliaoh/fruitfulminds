b87f3af057572650f80daf3b0222e9f70dfd862b
Sam <skx@berkeley.edu>
2013-11-29 11:53:17 -0800

    █

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -226,6 +226,11 @@ class ReportsController < ApplicationController
     data_list[0].keys.each do |q_id|
       questions += " " + Question.find_by_id(q_id).name
     end
+    values = ""
+    data_list[0].values.each do |v|
+      values += " " + v.to_s
+    end
+
     @curriculum.sections.each do |section_id|
       section = Section.find_by_id(section_id)
       next if section.stype != 'Multiple Choice'
@@ -233,14 +238,14 @@ class ReportsController < ApplicationController
       section_post_total = 0
       section.questions.each do |q_id|
         
-        if data_list[0][q_id].nil? or data_list[0][q_id].nil?
+        if data_list[0][q_id].nil? or data_list[1][q_id].nil?
           puts data_list[0][q_id]
           puts data_list[1][q_id]
           question = Question.find_by_id(q_id)
           puts question.msg
           puts question.name
           puts section.name
-          flash[:warning] = question.msg + question.name + section.name + "  " + questions
+          flash[:warning] = question.msg + question.name + section.name + "|||" + questions + "|||" + values
           redirect_to "/reports/new" and return
         end
         section_pre_total += data_list[0][q_id]
