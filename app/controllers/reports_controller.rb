4f0a230e8b221e9983f0add107749e4851715d8e
Sam <skx@berkeley.edu>
2013-11-28 22:59:26 -0800

    fwok

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -137,10 +137,13 @@ class ReportsController < ApplicationController
     efficacy_data = generate_data('Efficacy')
     objective_data = generate_data('Multiple Choice')
     if efficacy_data.nil? or objective_data.nil?
+      flash[:warning] = "Not enough data"
       redirect_to "/reports/new" and return
     elsif efficacy_data[0].keys.length != @questions['Efficacy'].length or efficacy_data[1].keys.length != @questions['Efficacy'].length
+      flash[:warning] = "Not enough data"
       redirect_to "/reports/new" and return
     elsif objective_data[0].keys.length != @questions['Multiple Choice'].length or objective_data[1].keys.length != @questions['Multiple Choice'].length
+      flash[:warning] = "Not enough data"
       redirect_to "/reports/new" and return
     end
 
