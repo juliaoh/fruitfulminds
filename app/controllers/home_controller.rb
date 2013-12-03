7fb4850b96a93c026b0bfe7e27b3503a73b3c16c
Kevin <kevinyeun@berkeley.edu>
2013-12-02 23:28:04 -0800

    fixed bugs and made prety

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,6 @@
 class HomeController < ApplicationController
+  include ActiveCoursesHelper
   def portal
-    courses = Course.all
-    if @current_user.admin?
-      courses = Course.all
-    else
-      courses = @current_user.courses
-    end
-    @active_courses = courses.select { |course| course.active == 0 }
-    @inactive_courses = courses.select { |course| course.active == 1 }
-    @active_courses.sort! do |a, b|
-      a.compare_name_and_semester(b)
-    end
-    @inactive_courses.sort_by! { |course| course.name }
+    get_active_inactive(@current_user)
   end
 end
