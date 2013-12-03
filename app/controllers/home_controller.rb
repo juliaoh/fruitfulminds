fe8aee1b7da5ca2aba4e73c779f8c2c3b26b6e23
Kevin <kevinyeun@berkeley.edu>
2013-12-02 22:28:48 -0800

    courses now inactivated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,9 @@ class HomeController < ApplicationController
       @active_courses = @current_user.courses.select { |course| course.active == 1 }
       @inactive_courses = @current_user.courses.select { |course| course.active == 0 }
     end
-    @active_courses.sort_by! { |course| course.name }
+    @active_courses.sort! do |a, b|
+      a.compare_name_and_semester(b)
+    end
     @inactive_courses.sort_by! { |course| course.name }
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -8,6 +8,8 @@ class Course < ActiveRecord::Base
 
   validates :active, :presence => true, :inclusion => [0,1]
 
+  after_find :check_active
+
 
   def name
     school.name + ", " + school.city + ", " + school.county + ", " + semester
@@ -19,5 +21,45 @@ class Course < ActiveRecord::Base
 
   def curriculum
     Curriculum.find(curriculum_id)
+  end
+
+  def check_active
+    if compare_semester(get_time_of_year) == 1
+      update_attributes!(:active => 0)
+    else
+      update_attributes!(:active => 1)
+    end
+  end
+
+  def get_time_of_year
+    if Time.now.year.month < 7
+      month = "Spring"
+    else
+      month = "Fall"
+    end
+    return "#{month} #{Time.now.year}"
+  end
+
+  def compare_name_and_semester(other)
+    temp = compare_semester(other.semester)
+    case
+    when temp == 0
+      name <=> other.name
+    else
+      temp
+    end
+  end
+
+  def compare_semester(other)
+    semesterA = semester.split(" ")
+    semesterB = other.split(" ")
+    case
+    when semesterA[1] < semesterB[1]
+      1
+    when semesterA[1] > semesterB[1]
+      -1
+    else
+      semesterA[0] <=> semesterB[0]
+    end
   end
 end
