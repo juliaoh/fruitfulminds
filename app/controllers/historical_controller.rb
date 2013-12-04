fa4b85ee394d0e505bd1f6dc891e79f4fab5af25
Michael Jun <stumichael9109@berkeley.edu>
2013-12-04 15:14:15 -0800

    historical in progress

--- app/controllers/historical_controller.rb
+++ app/controllers/historical_controller.rb
@@ -1,2 +1,14 @@
 class HistoricalController < ApplicationController
+  before_filter :admin_only
+
+  def new
+    @schools = School.all.collect { |s|
+      ["#{s.name}, #{s.city}, #{s.county}", s.id ] }.uniq.sort
+    @times = Course.all.collect { |t| ["#{t.semester}", t.id] }.uniq.sort
+  end
+
+  def create
+    @chosen_schools = School.find_by_id(params[:checked_schools])
+    chosen_times = params[:checked_times]
+  end
 end
