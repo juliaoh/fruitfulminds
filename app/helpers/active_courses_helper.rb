58ccf39882a82103f40d8b357369cd2605b7c96f
Kevin <kevinyeun@berkeley.edu>
2013-12-07 17:02:10 -0800

    admins courses works

--- app/helpers/active_courses_helper.rb
+++ app/helpers/active_courses_helper.rb
@@ -6,6 +6,15 @@ module ActiveCoursesHelper
     else
       courses = user.courses
     end
+    set_fields(courses)
+  end
+
+  def get_own_courses(user)
+    courses = user.courses
+    set_fields(courses)
+  end
+
+  def set_fields(courses)
     @active_courses = courses.select { |course| course.active == 1 and course.users.length > 0 }
     @inactive_courses = courses.select { |course| course.active == 0 and course.users.length > 0 }
     @active_courses.sort! do |a, b|
